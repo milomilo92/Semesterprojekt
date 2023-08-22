717021768d2ac7a032a06cab7478a80c1705f79a
Milos Milosavljevic <milos@milosavljevic.ch>
2023-08-22 11:51:33 +0200

    Loginfenster erstellt und programmiert

---
+++ ContactManager_ZBW/View_Cyril/Login.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ContactManager_ZBW.View_Cyril
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblUsername = new System.Windows.Forms.Label();
+            this.LblPassword = new System.Windows.Forms.Label();
+            this.TxtUsername = new System.Windows.Forms.TextBox();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.CmdLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LblUsername
+            // 
+            this.LblUsername.AutoSize = true;
+            this.LblUsername.Location = new System.Drawing.Point(13, 32);
+            this.LblUsername.Name = "LblUsername";
+            this.LblUsername.Size = new System.Drawing.Size(78, 13);
+            this.LblUsername.TabIndex = 0;
+            this.LblUsername.Text = "Benutzername:";
+            // 
+            // LblPassword
+            // 
+            this.LblPassword.AutoSize = true;
+            this.LblPassword.Location = new System.Drawing.Point(36, 66);
+            this.LblPassword.Name = "LblPassword";
+            this.LblPassword.Size = new System.Drawing.Size(55, 13);
+            this.LblPassword.TabIndex = 0;
+            this.LblPassword.Text = "Kennwort:";
+            // 
+            // TxtUsername
+            // 
+            this.TxtUsername.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.TxtUsername.Location = new System.Drawing.Point(98, 32);
+            this.TxtUsername.Name = "TxtUsername";
+            this.TxtUsername.Size = new System.Drawing.Size(145, 20);
+            this.TxtUsername.TabIndex = 1;
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.Location = new System.Drawing.Point(97, 59);
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.PasswordChar = '*';
+            this.TxtPassword.Size = new System.Drawing.Size(145, 20);
+            this.TxtPassword.TabIndex = 2;
+            // 
+            // CmdLogin
+            // 
+            this.CmdLogin.Location = new System.Drawing.Point(261, 32);
+            this.CmdLogin.Name = "CmdLogin";
+            this.CmdLogin.Size = new System.Drawing.Size(75, 47);
+            this.CmdLogin.TabIndex = 3;
+            this.CmdLogin.Text = "Login";
+            this.CmdLogin.UseVisualStyleBackColor = true;
+            this.CmdLogin.Click += new System.EventHandler(this.CmdLogin_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 111);
+            this.Controls.Add(this.CmdLogin);
+            this.Controls.Add(this.TxtPassword);
+            this.Controls.Add(this.TxtUsername);
+            this.Controls.Add(this.LblPassword);
+            this.Controls.Add(this.LblUsername);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblUsername;
+        private System.Windows.Forms.Label LblPassword;
+        private System.Windows.Forms.TextBox TxtUsername;
+        private System.Windows.Forms.TextBox TxtPassword;
+        private System.Windows.Forms.Button CmdLogin;
+    }
+}
---
+++ ContactManager_ZBW/View_Cyril/Login.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactManager_ZBW.View_Cyril
+{
+    public partial class Login : Form
+    {
+
+        private string[,] userCredentials = {
+            {"user", "1234"},
+            {"admin", "123456"}
+        };
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdLogin_Click(object sender, EventArgs e)
+        {
+            string enteredUsername = TxtUsername.Text;
+            string enteredPassword = TxtPassword.Text;
+            bool isAuthenticated = false;
+
+            for (int i = 0; i < userCredentials.GetLength(0); i++)
+            {
+                if (enteredUsername == userCredentials[i, 0] && enteredPassword == userCredentials[i, 1])
+                {
+                    isAuthenticated = true;
+                    break;
+                }
+            }
+
+            if (isAuthenticated)
+            {
+                MessageBox.Show("Anmeldung erfolgreich! Das Programm wird geladen.");
+                // Wie bringen wir den Load weiter, damit die Hauptview geladen wird?
+            }
+            else
+            {
+                MessageBox.Show("Falscher Benutzername oder Passwort. Bitte erneut versuchen.");
+            }
+        }
+    }
+
+}
